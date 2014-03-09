cd66851a5bb37e63ff5ac23cec97a51a4bdc80a6
wyicwx <wyicwx@gmail.com>
2014-03-09 17:04:14 +0800

    update delay stream unit-test

--- test/delay.js
+++ test/delay.js
@@ -61,4 +61,41 @@ describe('interval', function() {
 			}
 		}
 	});
+
+	it('设置延时时间, 输入间隔超过延时', function(done) {
+		var delayTime = 50;
+		var stream = delayStream(delayTime);
+		var timestamp = Date.now();
+		var availd = true;
+		var timeout = 60;
+
+		stream.on('data', function(chunk) {
+			if(Date.now() - timestamp > (timeout+10)) {
+				availd = false;
+			}
+			timestamp = Date.now(); 
+		});
+
+		stream.on('end', function() {
+			if(availd) {
+				done();
+			} else {
+				done(false);
+			}
+		});
+		var count = 20;
+		function next() {
+			stream.write(new Buffer(1));
+			count--;
+			if(!count) {
+				stream.end();
+			} else {
+				setTimeout(function() {
+					next();
+				}, timeout);
+			}
+		}
+
+		next();
+	});
 });
